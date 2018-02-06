3e31d90a9a7e3037559dab0bf17ba042c0a9d47f
Hiba Akroush <HAkroush@optimizasolutions.com>
2018-02-06 10:05:50 +0200

    dispacher redirection

--- src/main/java/com/in28minutes/springmvc/LoginController.java
+++ src/main/java/com/in28minutes/springmvc/LoginController.java
@@ -1,16 +1,30 @@
 package com.in28minutes.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import com.in28minutes.jee.userValidation;
 
 
 @Controller
 public class LoginController {
-	
-	@RequestMapping(value="/login")
-	@ResponseBody
-	public String sayHello(){
-		return "Hello World";
+	userValidation service = new userValidation();
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String showLoginPage(){
+		return "login";
 	}
+	@RequestMapping(value="/login", method = RequestMethod.POST)
+	public String handleLoginRequest(@RequestParam String name,@RequestParam String password, ModelMap model ){
+		model.put("password", password);
+		model.put("name", name);
+		if(service.isVAlid(name, password)){
+			return "welcome";			
+		}
+		model.put("error", "Wrong Username or Password");
+		return "login";
+	}	
 }
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -8,7 +8,7 @@
 </head>
 <body>
 
-<form action="/login.do" method="post">
+<form  method="POST">
 Enter Your Name <input type="text" name="name"/>
 Enter Your Password <input type="password" name="password"/>
 <input type="submit"/>
